c88adc8780248a257a8f2ddc7468672ba073881b
realsarius <54468228+realsarius@users.noreply.github.com>
2023-02-23 13:18:23 +0300

    pushing the final code

---
+++ imposter-app.js
@@ -0,0 +1,77 @@
+getText = () => {
+  fetch('test.txt')
+    .then((res) => {
+      if (res.ok) {
+        return res.text();
+      } else {
+        throw new Error('Something went wrong');
+      }
+    })
+    .then((data) => {
+      document.querySelector('#output').innerText = data;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+getJSON = () => {
+  fetch('posts.json')
+    .then((res) => {
+      if (res.ok) {
+        return res.json();
+      } else {
+        throw new Error('Something went wrong');
+      }
+    })
+    .then((data) => {
+      document.querySelector('#output').innerText = '';
+      const posts = data;
+      posts.forEach((post) => {
+        const title = document.createElement('h3');
+        const body = document.createElement('p');
+        const jsonDiv = document.createElement('div');
+        title.textContent = post['title'];
+        body.textContent = post['body'];
+        jsonDiv.appendChild(title);
+        jsonDiv.appendChild(body);
+        document.querySelector('#output').appendChild(jsonDiv);
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+getExternalData = () => {
+  fetch('https://api.github.com/users')
+    .then((res) => {
+      if (res.ok) {
+        return res.json();
+      } else {
+        throw new Error('Something went wrong');
+      }
+    })
+    .then((data) => {
+      console.log(data);
+      document.querySelector('#output').innerText = '';
+      data.forEach((user) => {
+        const userDiv = document.createElement('div');
+        const userLink = document.createElement('a');
+        userLink.href = `${user['html_url']}`;
+        userLink.setAttribute('target', '_blank');
+        userDiv.textContent = `User ${user['id']}: ${userLink.text}`;
+        userLink.textContent = `${user['login']}`;
+        userDiv.appendChild(userLink);
+        document.querySelector('#output').style.fontSize = '2rem';
+        document.querySelector('#output').appendChild(userDiv);
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+document.querySelector('#button1').addEventListener('click', getText);
+document.querySelector('#button2').addEventListener('click', getJSON);
+document.querySelector('#button3').addEventListener('click', getExternalData);
